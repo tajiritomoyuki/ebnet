7ca0d35e2da125ee5d95b5b52da01e5d77e5809f
tajiri <tajiri_leader@nagasebros.com>
2019-05-07 13:18:50 +0900

    ok

---
+++ create_csv.py
@@ -0,0 +1,19 @@
+#coding:utf-8
+import os
+import csv
+import glob
+
+from settings import *
+
+def main():
+    sector = 4
+    h5list = glob.glob(os.path.join(CTLdir, "*_%s_?_?.h5" % sector))
+    csvpath = os.path.join(csvdir, "CTL%s.csv" % sector)
+    with open(csvpath, "w") as f:
+        writer = csv.writer(f, linterminator="\n")
+        writer.writerow(["path"])
+        for h5path in h5list:
+            writer.writerow([os.path.basename(h5path)])
+
+if __name__ == '__main__':
+    main()
