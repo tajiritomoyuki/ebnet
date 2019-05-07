dce3f2f7acdb0c43dcc0861dbe9dab448d28ca23
tajiri <tajiri_leader@nagasebros.com>
2019-05-07 14:10:45 +0900

    ignore

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ import os
 tessroot = "/pike/pipeline"
 datroot = "C:\\Users\\tajiri\\Desktop\\tess\\data"
 # datroot = "/home/tomoyuki-tajiri/univ/tess/data"
-datroot = "/home/tajiri/tess/data"
+# datroot = "/home/tajiri/tess/data"
 
 rootdir = os.path.abspath(os.path.dirname(__file__))
 CTLdir = os.path.join(tessroot, "step3")
