ebdfd2a0a1b2653e0924858aa89b26ebe5720c6e
tomoyukitajiri <tomoyuki-tajiri@eps.s.u-tokyo.ac.jp>
2019-11-29 14:11:24 +0900

    1-16

--- create_csv.py
+++ create_csv.py
@@ -6,7 +6,7 @@ import glob
 from settings import *
 
 def main():
-    for sector in [14, 15, 16]:
+    for sector in range(1, 17):
         h5list = glob.glob(os.path.join(CTLdir, "*_%s_?_?.h5" % sector))
         csvpath = os.path.join(allcsvdir, "CTL%s.csv" % sector)
         with open(csvpath, "w") as f:
